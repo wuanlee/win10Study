40997d278cb55d3193184847de8f629d26cc2843
lenovo <lenovo@DESKTOP-JLL2MQK>
2016-04-17 14:08:15 +0800

    testWin10

--- LayoutStack/App.xaml.cs
+++ LayoutStack/App.xaml.cs
@@ -28,6 +28,9 @@ namespace LayoutStack
         /// </summary>
         public App()
         {
+            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
+                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
+                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
